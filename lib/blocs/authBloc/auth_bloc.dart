b771fc832f62db3fe43177494b075b434041494e
Saran-Kaanth <sarnknth16@gmail.com>
2023-07-01 23:24:39 +0530

    small changes in auth bloc

--- lib/blocs/authBloc/auth_bloc.dart
+++ lib/blocs/authBloc/auth_bloc.dart
@@ -1,13 +1,13 @@
 // import 'package:bloc/bloc.dart';
 // import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_mentor/data/repositories/auth_repository.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  // FirebaseAuth _firebaseAuth = FirebaseAuth.instance;   
   final AuthRepository authRepository;
 
   AuthBloc({required this.authRepository}) : super(AuthInitialState()) {
